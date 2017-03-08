49163a10df294ba73e1a871edb3e9029f33baddf
Dave Wasmer <davewasmer@gmail.com>
2017-03-08 08:52:10 -0700

    improve failure case when config is missing with assert message

--- config/initializers/objection-connect.ts
+++ config/initializers/objection-connect.ts
@@ -1,3 +1,4 @@
+import * as assert from 'assert';
 import * as knex from 'knex';
 import { Model } from 'objection';
 import { Application } from 'denali';
@@ -6,6 +7,7 @@ export default {
   name: 'objection-connect',
   async initialize(application: Application) {
     let adapter = application.container.lookup('orm-adapter:objection');
+    assert(application.config.database && application.config.database.client, 'Looks like you are missing database configuration. Add it to config.database - see the knex docs for configuration details: http://knexjs.org/#Installation-client');
     adapter.knex = knex(application.config.database);
     Model.knex(adapter.knex);
   }
