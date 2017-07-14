6edc6c294495abe4975825009c77253e2c91d571
Chris Watts <chris.watts.t@gmail.com>
2017-07-13 19:15:57 -0700

    Build

--- dist/config/initializers/objection-connect.d.ts
+++
@@ -1,7 +0,0 @@
-import { Application } from 'denali';
-declare const _default: {
-    name: string;
-    before: string;
-    initialize(application: Application): Promise<void>;
-};
-export default _default;
