072360db30deec474b0d11f0d54bda40acc59457
Chris Watts <chris.watts.t@gmail.com>
2017-10-03 08:09:06 -0700

    Add foreignKeyForRelationship

--- lib/relation-mappings/many-to-many.ts
+++ lib/relation-mappings/many-to-many.ts
@@ -1,13 +1,14 @@
 import * as assert from 'assert';
 import { Dict } from '../utils';
-import { camelCase } from 'lodash';
 import { Model as BaseObjectionModel } from 'objection';
 import ExtendedDenaliModel from '../denali-model';
 import ExtendedObjectionModel from '../objection-model';
 import { RelationshipDescriptor, Container } from 'denali';
 import { RelationMapping, RelationJoin, RelationThrough } from 'objection';
+import ObjectionAdapter from '../adapter';
 
 export default function generateManyToManyRelationMapping(
+  adapter: ObjectionAdapter,
   objectionModels: Dict<typeof ExtendedObjectionModel>,
   container: Container,
   model: typeof ExtendedDenaliModel,
@@ -18,17 +19,16 @@ export default function generateManyToManyRelationMapping(
   let options = descriptor.options;
   let ObjectionModel = objectionModels[type];
   let RelatedObjectionModel = objectionModels[descriptor.type];
-  let relatedType = RelatedObjectionModel.denaliModel.getType(container);
 
-  assert(ObjectionModel, `Unable to find the corresponding Objection model for the Denali "${ type }" model`);
-  assert(RelatedObjectionModel, `Unable to find the corresponding Objection model for the Denali "${ descriptor.type }" model`);
+  assert(ObjectionModel, `Unable to find the corresponding Objection model for the Denali "${type}" model`);
+  assert(RelatedObjectionModel, `Unable to find the corresponding Objection model for the Denali "${descriptor.type}" model`);
 
   let mapping = {
     relation: BaseObjectionModel.ManyToManyRelation,
     modelClass: RelatedObjectionModel,
     join: <RelationJoin>{
-      from: `${ ObjectionModel.tableName }.id`, // i.e. from: 'Post.id'
-      to: `${ RelatedObjectionModel.tableName }.id`, // i.e. to: 'Tag.id'
+      from: `${ObjectionModel.tableName}.id`, // i.e. from: 'Post.id'
+      to: `${RelatedObjectionModel.tableName}.id`, // i.e. to: 'Tag.id'
       through: <RelationThrough>{
         extra: options.manyToMany.extra
       }
@@ -40,11 +40,15 @@ export default function generateManyToManyRelationMapping(
     mapping.join.through.modelClass = objectionModels[options.manyToMany.model];
     joinTable = mapping.join.through.modelClass.tableName;
   } else {
-    joinTable = `${ ObjectionModel.tableName }_${ RelatedObjectionModel.tableName }`;
+    joinTable = `${ObjectionModel.tableName}_${RelatedObjectionModel.tableName}`;
   }
 
-  mapping.join.through.from = `${ joinTable }.${ camelCase(type) }Id`; // i.e. from: 'Post_Tag.postId'
-  mapping.join.through.to = `${ joinTable }.${ camelCase(relatedType) }Id`; // i.e. from: 'Post_Tag.tagId'
+  let foreignKeyForRelationship = options.foreignKeyForRelationship && typeof options.foreignKeyForRelationship === 'function' ?
+    options.foreignKeyForRelationship :
+    adapter.foreignKeyForRelationship;
+
+  mapping.join.through.from = `${joinTable}.${foreignKeyForRelationship(model)}`; // i.e. from: 'Post_Tag.postId'
+  mapping.join.through.to = `${joinTable}.${foreignKeyForRelationship(RelatedObjectionModel.denaliModel)}`; // i.e. from: 'Post_Tag.postId'
 
   return mapping;
 }
