26d02a36903c2226d521303cd409b8a82a522fde
Chris Watts <chris.watts.t@gmail.com>
2017-10-12 08:53:58 -0700

    Fix issue with setting related models

--- lib/adapter.ts
+++ lib/adapter.ts
@@ -118,8 +118,15 @@ export default class ObjectionAdapter extends ORMAdapter {
   async setRelated(model: ExtendedDenaliModel, relationship: string, descriptor: RelationshipDescriptor, relatedModels: ExtendedDenaliModel | ExtendedDenaliModel[]) {
     await model.record.$relatedQuery(relationship, this.testTransaction).unrelate();
 
-    let related = Array.isArray(relatedModels) ? relatedModels.map((relatedModel) => relatedModel.id) : relatedModels.id;
-    assert(related.filter(Boolean).length === related.length, 'You must pass Model instances to `setRelated()`, but one or more of instances of null or undefined were supplied. Make sure you are actually passing Model instances in.');
+    let related;
+    if (Array.isArray(relatedModels)) {
+      related = relatedModels.map((relatedModel) => relatedModel.id);
+      assert(related.filter(Boolean).length === related.length, 'You must pass Model instances to `setRelated()`, but one or more of instances of null or undefined were supplied. Make sure you are actually passing Model instances in.');
+    } else {
+      related = relatedModels.id;
+      assert(related, 'You must pass Model instance to `setRelated()`, but one instance of null or undefined were supplied. Make sure you are actually passing Model instance in.');
+    }
+
     return model.record.$relatedQuery(relationship, this.testTransaction).relate(related);
   }
 
@@ -163,15 +170,16 @@ export default class ObjectionAdapter extends ORMAdapter {
     }
   }
 
-  foreignKeyForRelationship(model: DenaliModel): string {
-    let type = model.getType(this.container);
+  foreignKeyForRelationship(relationship: RelationshipDescriptor): string {
+    // let type = model.getType(this.container);
+    let type = relationship.type;
 
     return `${camelCase(type)}Id`;
   }
 
   objectionModelForType(type: string): typeof ExtendedObjectionModel {
     let AdapterObjectionModel = this.objectionModels[type];
-    assert(AdapterObjectionModel, `Unable to locate the objection.js model class for the ${ type } type. Available objection models: ${ Object.keys(this.objectionModels).join(', ') }`);
+    assert(AdapterObjectionModel, `Unable to locate the objection.js model class for the ${type} type. Available objection models: ${Object.keys(this.objectionModels).join(', ')}`);
     if (this.testTransaction) {
       return AdapterObjectionModel.bindTransaction(this.testTransaction);
     }
